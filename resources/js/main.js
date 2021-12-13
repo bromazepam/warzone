5cfd3db56a6de27b9d4737656044fc5f549290a4
bromazepam <graor95@gmail.com>
2021-12-13 19:04:14 +0100

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -24,116 +24,7 @@ const locations = [
         top: 391,
         left: 252,
     },
-    // {
-    //     name: "Beachhead",
-    //     top: 332,
-    //     left: 679,
-    // },
-    // {
-    //     name: "Military Base",
-    //     top: 147,
-    //     left: 474,
-    // },
-    // {
-    //     name: "Salt Mine",
-    //     top: 198,
-    //     left: 633,
-    // },
-    // {
-    //     name: "Array",
-    //     top: 339,
-    //     left: 599,
-    // },
-    // {
-    //     name: "TV Station",
-    //     top: 366,
-    //     left: 521,
-    // },
-    // {
-    //     name: "Air Port",
-    //     top: 351,
-    //     left: 273,
-    // },
-    // {
-    //     name: "Super Store",
-    //     top: 399,
-    //     left: 329,
-    // },
-    // {
-    //     name: "Factory",
-    //     top: 428,
-    //     left: 399,
-    // },
-    // {
-    //     name: "Storage Town",
-    //     top: 428,
-    //     left: 246,
-    // },
-    // {
-    //     name: "Stadium",
-    //     top: 449,
-    //     left: 612,
-    // },
-    // {
-    //     name: "Lumber",
-    //     top: 449,
-    //     left: 756,
-    // },
-    // {
-    //     name: "Hospital",
-    //     top: 531,
-    //     left: 458,
-    // },
-    // {
-    //     name: "Boneyard",
-    //     top: 530,
-    //     left: 236,
-    // },
-    // {
-    //     name: "Train Station",
-    //     top: 560,
-    //     left: 344,
-    // },
-    // {
-    //     name: "Downtown",
-    //     top: 566,
-    //     left: 576,
-    // },
-    // {
-    //     name: "Farmland",
-    //     top: 557,
-    //     left: 734,
-    // },
-    // {
-    //     name: "Promenade East",
-    //     top: 614,
-    //     left: 431,
-    // },
-    // {
-    //     name: "Promenade West",
-    //     top: 638,
-    //     left: 281,
-    // },
-    // {
-    //     name: "Hills",
-    //     top: 699,
-    //     left: 342,
-    // },
-    // {
-    //     name: "Park",
-    //     top: 675,
-    //     left: 548,
-    // },
-    // {
-    //     name: "Port",
-    //     top: 653,
-    //     left: 657,
-    // },
-    // {
-    //     name: "Prison",
-    //     top: 732,
-    //     left: 756,
-    // },
+    //todo add all locations
 ];
 
 
@@ -147,6 +38,7 @@ const IMAGE_HEIGHT = 856;
 pin.style.visibility= 'hidden';
 
 jump.addEventListener("click", () => {
+    //todo make random location non repeating
     let location = locations[Math.floor(Math.random()*locations.length)];
     result.innerText = location.name;
 
