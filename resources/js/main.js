36543595114e9619e2278b5f4c38a0f44fd5c977
bromazepam <graor95@gmail.com>
2021-12-14 11:42:06 +0100

    added all locations

--- resources/js/main.js
+++ resources/js/main.js
@@ -24,9 +24,172 @@ const locations = [
         top: 391,
         left: 252,
     },
-    //todo add all locations
+    {
+        name: "Docks",
+        top: 120,
+        left: 489,
+    },
+    {
+        name: "Docks",
+        top: 50,
+        left: 480,
+    },
+    {
+        name: "Docks",
+        top: 120,
+        left: 572,
+    },
+    {
+        name: "Runway",
+        top: 193,
+        left: 683,
+    },
+    {
+        name: "Runway",
+        top: 142,
+        left: 699,
+    },
+    {
+        name: "Mines",
+        top: 270,
+        left: 276,
+    },
+    {
+        name: "Mines",
+        top: 182,
+        left: 444,
+    },
+    {
+        name: "Mines",
+        top: 252,
+        left: 438,
+    },
+    {
+        name: "Mines",
+        top: 302,
+        left: 362,
+    },
+    {
+        name: "Mines",
+        top: 397,
+        left: 351,
+    },
+    {
+        name: "Peak",
+        top: 322,
+        left: 507,
+    },
+    {
+        name: "Beachhead",
+        top: 322,
+        left: 672,
+    },
+    {
+        name: "Beachhead",
+        top: 246,
+        left: 694,
+    },
+    {
+        name: "Beachhead",
+        top: 364,
+        left: 713,
+    },
+    {
+        name: "Beachhead",
+        top: 303,
+        left: 607,
+    },
+    {
+        name: "Village",
+        top: 359,
+        left: 158,
+    },
+    {
+        name: "Village",
+        top: 413,
+        left: 154,
+    },
+    {
+        name: "Village",
+        top: 464,
+        left: 191,
+    },
+    {
+        name: "Village",
+        top: 467,
+        left: 270,
+    },
+    {
+        name: "Lagoon",
+        top: 517,
+        left: 97,
+    },
+    {
+        name: "Lagoon",
+        top: 561,
+        left: 157,
+    },
+    {
+        name: "Lagoon",
+        top: 517,
+        left: 180,
+    },
+    {
+        name: "Power Plant",
+        top: 676,
+        left: 363,
+    },
+    {
+        name: "Capital",
+        top: 736,
+        left: 460,
+    },
+    {
+        name: "Capital",
+        top: 731,
+        left: 585,
+    },
+    {
+        name: "Resort",
+        top: 720,
+        left: 679,
+    },
+    {
+        name: "Airfield",
+        top: 562,
+        left: 260,
+    },
+    {
+        name: "Airfield",
+        top: 612,
+        left: 262,
+    },
+    {
+        name: "Fields",
+        top: 530,
+        left: 410,
+    },
+    {
+        name: "Fields",
+        top: 492,
+        left: 522,
+    },
+    {
+        name: "Fields",
+        top: 592,
+        left: 547,
+    },
+    {
+        name: "Fields",
+        top: 505,
+        left: 647,
+    },
+    {
+        name: "Sub Pen",
+        top: 571,
+        left: 720,
+    },
 ];
-
 
 const pin = document.getElementById("ping");
 const jump = document.getElementById("whereTo");
